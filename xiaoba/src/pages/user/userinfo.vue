3f120b21c94ee6f83dde2417b4b5d5153e1a58b9
aberl <aberl-workload@163.com>
2019-06-16 21:47:53 +0800

    set user role

--- xiaoba/src/pages/user/userinfo.vue
+++ xiaoba/src/pages/user/userinfo.vue
@@ -2,7 +2,7 @@
   <div>
     <headerTop/>
     <div class="container">
-      <div class="my-3 p-3 bg-white rounded box-shadow">
+      <div class="my-3 p-3 bg-white rounded box-shadow title_font">
         个人信息&nbsp;&nbsp;
         <img src="./images/driver64.png">
 
@@ -13,13 +13,29 @@
           <el-form-item label="手机号">&#8195;&#8195;15001112627</el-form-item>
           <el-divider></el-divider>
           <el-form-item label="会员等级">
-            &#8195;&#8195;
-            <el-radio-group v-model="sizeForm.resource" size="medium">
-              <el-radio border label="游客"></el-radio>
+            &#8195;&#8195;当前身份&#8195;&#8195; <el-button type="success">游&#8195;客</el-button>
+            <div class="padding-top"></div>
+            &#8195;&#8195;升级身份&#8195;&#8195;
+            <el-radio-group fill="red"  v-model="sizeForm.resource" size="medium">
+              <el-radio border label="游 客"></el-radio>
               <el-radio border label="普通会员"></el-radio>
               <el-radio border label="VIP会员"></el-radio>
             </el-radio-group>
           </el-form-item>
+          <el-divider></el-divider>
+          <el-form-item label="当前特权">
+              &#8195;&#8195;每日免费观看3段视频
+            <div class="padding-top"></div>
+              &#8195;&#8195;无限制观看视频
+            <div class="padding-top"></div>
+              &#8195;&#8195;无限制下载视频
+            <div class="padding-top"></div>
+              &#8195;&#8195;视频快进播放
+            <div class="padding-top"></div>
+              &#8195;&#8195;在线提交评论
+            <div class="padding-top"></div>
+              &#8195;&#8195;在线向老师提问
+              </el-form-item>
         </el-form>
       </div>
     </div>
@@ -34,6 +50,7 @@ import footerGuide from "../../components/Footer/footer.vue";
 export default {
   data() {
     return {
+    radioSelectedColor:"#67C23A",
       labelPosition: "top",
       formLabelAlign: {
         name: "",
@@ -63,6 +80,9 @@ export default {
 <style>
 .title_font {
   font-family: "微软雅黑";
-  font-size: 20px;
+  font-size: 30px;
+}
+.padding-top {
+  padding-top: 20px;
 }
 </style>
