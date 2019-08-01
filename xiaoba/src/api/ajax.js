677d7315486ad1ea8ceda77d433f1ba86f8e020f
aberl <aberl-workload@163.com>
2019-08-01 22:09:12 +0800

    add global header for authorization token in request

--- xiaoba/src/api/ajax.js
+++ xiaoba/src/api/ajax.js
@@ -1,39 +1,47 @@
-import axios from 'axios'
+import axios from "axios";
 
-export default function ajax(url = '', data = {}, type = 'GET', config) {
-  return new Promise(function (resolve, reject) {
+axios.interceptors.request.use(
+  config => {
+    config.headers.AuthorizationToken = "xxxxxx";
+    return config;
+  },
+  err => {
+    return Promise.reject(err);
+  }
+);
 
-    let promise
+export default function ajax(url = "", data = {}, type = "GET", config) {
+  return new Promise(function(resolve, reject) {
+    let promise;
 
-    if (type === 'GET') {
+    if (type === "GET") {
       // 准备url query参数数据
-      let dataStr = '' //数据拼接字符串
+      let dataStr = ""; //数据拼接字符串
       Object.keys(data).forEach(key => {
-        dataStr += key + '=' + data[key] + '&'
-      })
-      if (dataStr !== '') {
-        dataStr = dataStr.substr(0, dataStr.lastIndexOf('&'))
-        url = url + '?' + dataStr
+        dataStr += key + "=" + data[key] + "&";
+      });
+      if (dataStr !== "") {
+        dataStr = dataStr.substr(0, dataStr.lastIndexOf("&"));
+        url = url + "?" + dataStr;
       }
       // 发送get请求
-      promise = axios.get(url)
-    } else if (type === 'POST') {
+      promise = axios.get(url, config);
+    } else if (type === "POST") {
       // 发送post请求
-      promise = axios.post(url, data,config)
-    }
-    else if (type === 'PUT') {
+      promise = axios.post(url, data, config);
+    } else if (type === "PUT") {
       // 发送post请求
-      promise = axios.put(url, data)
-    }
-    else if (type === 'DELETE') {
+      promise = axios.put(url, data);
+    } else if (type === "DELETE") {
       // 发送post请求
-      promise = axios.delete(url, {data})
+      promise = axios.delete(url, { data });
     }
-    promise.then(response => {
-      resolve(response.data)
-    })
-      .catch(error => {
-        reject(error)
+    promise
+      .then(response => {
+        resolve(response.data);
       })
-  })
+      .catch(error => {
+        reject(error);
+      });
+  });
 }
